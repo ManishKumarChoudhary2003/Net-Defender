a3825b3e6a2578ad9cffb5c82973f5b7b470947b
Manish Kumar Choudhary <manish@platformcommons.com>
2024-09-23 21:58:47 +0530

    first commit

---
+++ src/main/java/com/net_defender/NetDefenderApplication.java
@@ -0,0 +1,13 @@
+package com.net_defender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetDefenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetDefenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/net_defender/NetDefenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net_defender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetDefenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
